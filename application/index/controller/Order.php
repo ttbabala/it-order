e71e7843da4fe516f3e95aa8b4ca966f1a9597f5
ttbabala <hqiotzx@126.com>
2017-05-08 23:57:38 +0800

    tp5

---
+++ application/index/controller/Order.php
@@ -0,0 +1,16 @@
+<?php
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+namespace app\index\controller;
+use think\Controller;
+
+class Order extends Controller
+{
+    public function index(){
+        echo 'this is Order controller!';
+    }
+}
+
