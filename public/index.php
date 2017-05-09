8b8e006192eacc6de45df9ed5d40064945a46bd7
ttbabala <hqiotzx@126.com>
2017-05-09 17:30:52 +0800

    ver1

--- public/index.php
+++ public/index.php
@@ -12,8 +12,6 @@
 // [ 应用入口文件 ]
 
 // 定义应用目录
-define('APP_PATH', './application/');
-// 开启调试模式
-define('APP_DEBUG', true);
+define('APP_PATH', __DIR__ . '/../application/');
 // 加载框架引导文件
-require './thinkphp/start.php';
+require __DIR__ . '/../thinkphp/start.php';
